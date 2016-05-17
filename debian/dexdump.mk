6547abcd098f367ba38a0d11caf943e9c5db02c9
Kai-Chung Yan <seamlikok@gmail.com>
2016-05-17 19:01:42 +0800

    d/dexdump.mk: Fix typo and clean up

--- debian/dexdump.mk
+++ debian/dexdump.mk
@@ -1,9 +1,9 @@
 NAME = dexdump
 SOURCES = DexDump.cpp
 SOURCES := $(foreach source, $(SOURCES), dexdump/$(source))
-CPPFLAGS += -fPIC
+CXXFLAGS += -fPIC
 CPPFLAGS += -include android/arch/AndroidConfig.h -I/usr/include/android -I.
-LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android:/usr/lib/android -L. -ldex
+LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android -L. -ldex
 
 build: $(SOURCES)
 	$(CXX) $^ -o $(NAME)/$(NAME) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
